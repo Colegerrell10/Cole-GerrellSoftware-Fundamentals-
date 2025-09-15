4254d076ba87f1eebc41e9dc1e704987e7ca2751
poppiejosh <poppiejoshua@gmail.com>
2025-09-15 17:00:05 -0500

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,26 +1,27 @@
 package demos.junitdemo;
 
 public class BinarySearch {
-	/*int[] array;
+	int[] array;
 	int min = 0;
-	int max = array.length;*/
-	public int calcMiddle(int min, int max, int target) {
-		int middle = max - ((max - min)/2);
-		if(middle == target) {
-			return target;
-		}else if(middle < target) {
-			return 1;
-		}else{
+	int max = array.length;
+	public static int binarySearch(int[] array, int min, int max, int target) {
+		if(array.length == 0 && array[0] != target){
 			return -1;
 		}
+		int middle = max - ((max - min)/2);
+		if(target > middle) {
+			return binarySearch(array, middle, max, target);
+		}else if(target < middle) {
+			return binarySearch(array, min, middle, target); 
+		}else {
+			return target;
+		}
 	}
-	public static int binarySearch(int[] array, int target) {
-		int cond= -1;
-		
-		//max = array.length;
-		//int min= 0;
-		// TODO Auto-generated constructor stub
-		return cond;
+	//delete once we create test cases?
+	public static void main (String args[]) {
+		int[] sampleArray1 = {1,2,3,4};
+		int ex1 = binarySearch(sampleArray1, 1, 4, 3);
+		System.out.println(ex1);
 	}
 
 }
